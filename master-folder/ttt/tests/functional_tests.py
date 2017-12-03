98db52875036f4c7362ec4a36c153e65ed1313f9
SimonTanner <simonthetanner@hotmail.com>
2017-12-03 19:37:21 +0000

    Adds new test to functional tests to enter the players name and find their name on the page

--- master-folder/ttt/tests/functional_tests.py
+++ master-folder/ttt/tests/functional_tests.py
@@ -25,12 +25,21 @@ class NewGametest(unittest.TestCase):
         self.assertEqual(self.browser.current_url, 'https://github.com/simontanner/machine-learning')
         #button = self.browser.find_element_by_name("Play ML TTT")
 
-    def test_button(self):
+    def test_play_button(self):
         self.browser.get('http://localhost:8000')
         button = self.browser.find_element_by_name("playGame")
         button.click()
         header = self.browser.find_element_by_tag_name('li').text
         self.assertEqual('Play the Machine', header)
+
+    def test_can_enter_their_name(self):
+        self.browser.get('http://localhost:8000/newgame')
+        submit_name = self.browser.find_element_by_tag_name("submitName")
+        submit_name.send_keys('Simon')
+        submit_name.send_keys(Keys.ENTER)
+        time.sleep(10)
+        header = self.browser.find_element_by_tag_name('li').text
+        self.assertEqual('Simon vs. Machine', header)
 
 
 if __name__ == '__main__':
