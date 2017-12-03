4a37b1a57944e880270d4959da624155638a73ae
SimonTanner <simonthetanner@hotmail.com>
2017-12-03 17:10:39 +0000

    Changed landing page and styling and functional tests. Button added without link as yet

--- master-folder/ttt/tests/functional_tests.py
+++ master-folder/ttt/tests/functional_tests.py
@@ -14,13 +14,16 @@ class NewGametest(unittest.TestCase):
     def test_home_page(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('ML-TTT', self.browser.title)
-        
+        header = self.browser.find_element_by_tag_name('li').text
+        self.assertEqual("Welcome to TicTacToe Man vs. Machine!", header)
+
 
     def test_links(self):
         self.browser.get('http://localhost:8000')
-        image = self.browser.find_element_by_xpath("//img[@title='logo']")
+        image = self.browser.find_element_by_xpath("//img[@title='github']")
         image.click()
         self.assertEqual(self.browser.current_url, 'https://github.com/simontanner/machine-learning')
+        #button = self.browser.find_element_by_name("Play ML TTT")
 
 
 if __name__ == '__main__':
