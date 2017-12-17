12709199cba0967aa37344b4033b6a2690e08d03
SimonTanner <simonthetanner@hotmail.com>
2017-12-17 17:23:05 +0000

    Test added to game_test for an instance of TicTacToe class being instantiated when a game is started

--- master-folder/ttt/game_test.py
+++ master-folder/ttt/game_test.py
@@ -1,5 +1,6 @@
 import unittest
 from lib.game import Game
+from lib.ttt_board import TicTacToe
 
 class GameTest(unittest.TestCase):
 
@@ -9,7 +10,12 @@ class GameTest(unittest.TestCase):
     def test_a_new_game_is_started_the_player_must_enter_a_name(self):
         self.game = Game('Michelle')
         self.assertEqual(self.game.player_name, 'Michelle')
-    
+
+    def test_a_board_is_created_when_a_new_game_starts(self):
+        self.game = Game('Michelle')
+        board = self.game.board
+        self.assertIsInstance(TicTacToe, board)
+
 
 if __name__ == '__main__':
     unittest.main()
