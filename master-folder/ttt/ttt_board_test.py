5cacde0605187ffd5bbc639019759a695a17fcb3
SimonTanner <simonthetanner@hotmail.com>
2017-12-18 14:12:25 +0000

    Test added to ttt_boarrd_test to see if a player can choose a space and have this change the value of the board dict.

--- master-folder/ttt/ttt_board_test.py
+++ master-folder/ttt/ttt_board_test.py
@@ -4,15 +4,20 @@ from lib.ttt_board import TicTacToe
 class TTTTest(unittest.TestCase):
 
     def setUp(self):
+        self.ttt = TicTacToe()
         pass
 
     def test_TTT_contains_a_dict_with_keys_1_to_9(self):
-        ttt = TicTacToe()
+
         ttt_test_board = {}
         for i in range(1, 10):
             ttt_test_board[str(i)] = ' '
 
-        self.assertEqual(ttt.board, ttt_test_board)
+        self.assertEqual(self.ttt.board, ttt_test_board)
+
+    def test_TTT_allows_player_to_choose_a_space(self):
+        self.ttt.choose_space('6', 'X')
+        self.assertLessEqual({'6':'X'}.items(), self.ttt.board.items())
 
 
 if __name__ == '__main__':
