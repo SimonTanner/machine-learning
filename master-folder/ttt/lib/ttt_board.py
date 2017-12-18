8a4022309181bd0842317e9cc58c046c28c61736
SimonTanner <simonthetanner@hotmail.com>
2017-12-18 13:35:41 +0000

    ttt_board_test fixed

--- master-folder/ttt/lib/ttt_board.py
+++ master-folder/ttt/lib/ttt_board.py
@@ -1,3 +1,7 @@
 
 
 class TicTacToe():
+
+    def __init__(self):
+        self.board = {}
+        
--- master-folder/ttt/ttt_board_test.py
+++ master-folder/ttt/ttt_board_test.py
@@ -1,15 +1,19 @@
 import unittest
 from lib.ttt_board import TicTacToe
 
-class TTTTest(self):
+class TTTTest(unittest.TestCase):
 
     def setUp(self):
         pass
 
     def test_TTT_contains_a_dict_with_keys_1_to_9(self):
-        ttt = TicTaToe()
+        ttt = TicTacToe()
         ttt_test_board = {}
         for i in range(1, 10):
             ttt_test_board[str(i)] = ' '
 
-        assertEqual(ttt.board, ttt_test_board)
+        self.assertEqual(ttt.board, ttt_test_board)
+
+
+if __name__ == '__main__':
+    unittest.main()
