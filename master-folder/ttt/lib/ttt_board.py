00fee89bc2a0f1fb720c27073298ca9024cefbd8
SimonTanner <simonthetanner@hotmail.com>
2017-12-18 13:38:48 +0000

    Passes test for board having dict with keys 1 - 9

--- master-folder/ttt/lib/ttt_board.py
+++ master-folder/ttt/lib/ttt_board.py
@@ -4,4 +4,9 @@ class TicTacToe():
 
     def __init__(self):
         self.board = {}
-        
+        self.create_board()
+
+
+    def create_board(self):
+        for i in range(1, 10):
+            self.board[str(i)] = ' '
