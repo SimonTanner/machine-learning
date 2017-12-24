9be1174e326035ac2ad5346f86d379b9edc96563
SimonTanner <simonthetanner@hotmail.com>
2017-12-24 16:55:45 +0000

    Passes test

--- master-folder/ttt/lib/game.py
+++ master-folder/ttt/lib/game.py
@@ -4,8 +4,9 @@ import random
 
 class Game():
 
-    def __init__(self, player_name):
+    def __init__(self, player_name, x_or_o='X'):
         self.player_name = player_name
+        self.player_char = x_or_o
         self.board = TicTacToe()
         self.machine_player = MachinePlayer()
         self.players = [player_name, 'machine_player']
@@ -13,3 +14,6 @@ class Game():
 
     def who_goes_first(self):
         self.whose_turn = random.choice(self.players)
+
+    def choose_space(self, choice):
+        self.board.choose_space(str(choice), self.player_char)
