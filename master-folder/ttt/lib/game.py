3efdaf67e5b30f1926f9ac260db1d40daf3ded57
SimonTanner <simonthetanner@hotmail.com>
2017-12-24 17:31:40 +0000

    Refactored game to have a dictionary containing the player names as keys and their characters i.e. '0' or 'X'

--- master-folder/ttt/lib/game.py
+++ master-folder/ttt/lib/game.py
@@ -5,15 +5,16 @@ import random
 class Game():
 
     def __init__(self, player_name, x_or_o='X'):
+        chars = {'X':'0', '0':'X'}
         self.player_name = player_name
-        self.player_char = x_or_o
+        machine_player_char = chars[x_or_o]
         self.board = TicTacToe()
         self.machine_player = MachinePlayer()
-        self.players = [player_name, 'machine_player']
+        self.players = {player_name: x_or_o, 'machine_player':machine_player_char}
         self.who_goes_first()
 
     def who_goes_first(self):
-        self.whose_turn = random.choice(self.players)
+        self.whose_turn = random.choice(list(self.players.keys()))
 
     def choose_space(self, choice):
-        self.board.choose_space(str(choice), self.player_char)
+        self.board.choose_space(str(choice), self.players[self.whose_turn])
