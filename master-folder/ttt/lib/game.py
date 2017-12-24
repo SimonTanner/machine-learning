fd768f2f2854ffe06df676ec0d3da5e6edaa7804
SimonTanner <simonthetanner@hotmail.com>
2017-12-24 13:14:11 +0000

    Passes test

--- master-folder/ttt/lib/game.py
+++ master-folder/ttt/lib/game.py
@@ -1,7 +1,9 @@
 from lib.ttt_board import TicTacToe
+from lib.machine_model import MachinePlayer
 
 class Game():
 
     def __init__(self, player_name):
         self.player_name = player_name
         self.board = TicTacToe()
+        self.machine_player = MachinePlayer()
