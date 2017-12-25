a22c641e2cd91e5015056de3bde44fe63629f69f
SimonTanner <simonthetanner@hotmail.com>
2017-12-25 15:33:03 +0000

    Test written for variable which contains a list of free spaces in ttt_board_test

--- master-folder/ttt/ttt_board_test.py
+++ master-folder/ttt/ttt_board_test.py
@@ -23,6 +23,10 @@ class TTTTest(unittest.TestCase):
         self.ttt.choose_space('6', 'X')
         self.assertEqual(self.ttt.choose_space('6', 'X'), True)
 
+    def test_when_a_player_chooses_a_space_it_is_dropped_from_free_spaces(self):
+        self.ttt.choose_space('6', 'X')
+        self.assertNotIn(6, self.ttt.free_spaces)
+
 
 if __name__ == '__main__':
     unittest.main()
