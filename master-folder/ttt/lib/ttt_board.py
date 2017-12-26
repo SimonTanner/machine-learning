395c9fe7f68cc0859859f79cc2be7e64b6bd86a4
SimonTanner <simonthetanner@hotmail.com>
2017-12-26 13:35:04 +0000

    Passes ttt_board test

--- master-folder/ttt/lib/ttt_board.py
+++ master-folder/ttt/lib/ttt_board.py
@@ -5,6 +5,7 @@ class TicTacToe():
     def __init__(self):
         self.board = {}
         self.free_spaces = []
+        self.win = False
         self.create_board()
 
 
@@ -13,11 +14,18 @@ class TicTacToe():
             self.board[str(i)] = ' '
             self.free_spaces.append(i)
 
+    def check_board(self):
+        board = self.board
+        for i in range(1, 9, 3):
+            if board[str(i)] == board[str(i + 1)] == board[str(i + 2)]:
+                self.win = True
+
     def choose_space(self, space, character):
         space_taken = False
         if self.board[space] == ' ':
             self.board[space] = character
             self.free_spaces.remove(int(space))
+            self.check_board()
         else:
             space_taken = True
 
